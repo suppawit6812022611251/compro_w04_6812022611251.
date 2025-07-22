f7ad9baa689b8bc6f56284430d86cf3e39fa3f60
suppawit6812022611251 <s6812022611251@email.kmutnb.ac.th>
2025-07-22 16:14:57 +0700

    Add files via upload

---
+++ coding_w04-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char name = 'S';
+    int ag = 18;
+    float wei = 83.50;
+    char gen[] = "Male";
+
+    printf("nameInitial: %c\n", name);
+    printf("age: %u\n" , ag);
+    printf("weight: %f\n" , wei);
+    printf("gender: %s\n" , gen);
+
+    return 0;
+}
